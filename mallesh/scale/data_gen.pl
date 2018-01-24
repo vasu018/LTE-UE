4f2f8520eae32bf33ff4fcd94b32aceb675bf806
Vasudevan Poornima <vasudevan@Vasudevans-MacBook-Pro.local>
2018-01-24 10:59:00 -0500

    scale

--- mallesh/scale/data_gen.pl
+++ mallesh/scale/data_gen.pl
@@ -2,24 +2,31 @@
 
 my $serial = 0;
 my $data1 = 0;
-open (OUTPUTFILE, "> ./scale_data.txt");
+my $data2 = 0;
+open (OUTPUTFILE1, "> ./scale_data.txt");
+open (OUTPUTFILE2, "> ./nf_scale_data.txt");
 my $i =0;
-my $totalrange = 1200;
-for ($i =0; $i <1200; $i++) {
+my $totalrange = 300;
+for ($i =0; $i <300; $i++) {
 
     if ($i >= $totalrange/2) {
         if ($i % 5 eq 0) {
-            $data1 = $data1 - 100;
+            $data1 = $data1 - 400;
+            $data2 = $data2 - 400;
         }
     }
     else {
         if ($i % 5 eq 0) {
-            $data1 = $data1 + 100;
+            $data1 = $data1 + 400;
+            $data2 = $data2 + 400 + rand(2);
         }
     }
 
-    print OUTPUTFILE "$serial, $data1\n";
-    print "$serial,$data1\n";
+    print OUTPUTFILE1 "$serial, $data1\n";
+    print OUTPUTFILE2 "$serial, $data2\n";
+    #print "$serial,$data1\n";
+    #print "$serial,$data2\n";
     $serial = $serial +1;
 }
-close(OUTPUTFILE);
+close(OUTPUTFILE1);
+close(OUTPUTFILE2);
