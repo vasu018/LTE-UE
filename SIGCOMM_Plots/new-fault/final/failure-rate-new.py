d718779c364ffae70f4e27a8efddf51afde4a391
Vasudevan Nagendra <vasu.018@gmail.com.com>
2018-06-10 21:15:51 -0700

    code changes to failure

--- SIGCOMM_Plots/new-fault/final/failure-rate-new.py
+++ SIGCOMM_Plots/new-fault/final/failure-rate-new.py
@@ -14,7 +14,8 @@ f.subplots_adjust(left=0.1, right=0.99)
 tr = [10000, 20000, 30000, 40000, 50000]
 tr = [i/1000 for i in tr]
 
-sf = [9.35, 13.25, 19.12, 20.95, 23.45]
+#sf = [9.35, 13.25, 19.12, 20.95, 23.45]
+sf = [7.35, 11.25, 19.12, 27.95, 43.45]
 sl_cold = [0.75, 1.28, 2.35, 3.62, 4.75]
 sl_hot = [0.66, 1.12, 1.74, 2.39, 2.57]
 
@@ -22,13 +23,13 @@ plt.plot(tr, sf, linewidth=5, color='magenta', ls='--', marker='^', markersize=1
 plt.plot(tr, sl_cold, linewidth=5, color='maroon', ls='--', marker='s', markersize=14, label='Stateless Cold Migration')
 plt.plot(tr, sl_hot, linewidth=5, color='blue', ls='--', marker='D', markersize=14, label='Stateless Hot Migration')
 
-plt.xlabel('# Control Procedures Per Second')
-plt.ylabel('% Connection Reattaches/Failures')
+plt.xlabel('# Control Procedures/Second')
+plt.ylabel('Connection Failures (%)')
 
 plt.xticks(tr, ['10k', '20k', '30k', '40k', '50k'])
-plt.ylim([-1, 30])
-
+plt.ylim([-1, 50])
+plt.yticks(range(0, 51, 10))
 plt.legend(loc='upper left')
 plt.grid(linestyle='--')
-plt.savefig("./failure_rate.pdf", bbox_inches='tight')
+plt.savefig("./failure_rate_new.pdf", bbox_inches='tight')
 plt.show()
