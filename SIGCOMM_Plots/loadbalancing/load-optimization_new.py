09a422d4d53d04b728d3bdacb78b8e1e29bcd048
Vasudevan Poornima <vasu.018@gmail.com>
2018-06-16 16:11:19 -0700

    load optimization code changes

--- SIGCOMM_Plots/loadbalancing/load-optimization_new.py
+++ SIGCOMM_Plots/loadbalancing/load-optimization_new.py
@@ -4,28 +4,16 @@ import matplotlib.pyplot as plt
 import itertools
 from collections import OrderedDict
 from functools import partial
+from matplotlib import gridspec
 
-csfont = {'fontname':'Comic Sans MS', 'fontsize':'52'}
-csfont2 = {'fontname':'Comic Sans MS', 'fontsize':'42'}
-
-csfont3 = {'fontname':'Comic Sans MS', 'fontsize':'52'}
-
-hfont = {'fontname':'Helvetica', 'fontsize':'24'}
+hfont = {'fontname':'Helvetica', 'fontsize':'48'}
 dashList = [(5,2),(2,5),(4,10),(3,3,2,2),(5,5,20,5)]
 
-matplotlib.rcParams.update({'font.size':36})
+matplotlib.rcParams.update({'font.size':48})
 matplotlib.rcParams['figure.figsize'] = 14, 10
 
-
 fig, (ax1, ax2) = plt.subplots(ncols=2)
-fig.tight_layout()
 fig.subplots_adjust(left=0.07, right=0.99)
-
-
-#params = {'legend.fontsize': 28,
-#          'legend.handlelength': 2,
-#          'legend.ncols':2}
-#plt.rcParams.update(params)
 
 N1 = 4 
 rr = [6.2, 5.3, 3.6, 6.7]
@@ -41,31 +29,29 @@ sk_ch_sim = [57]
 
 ## necessary variables
 ind1 = np.arange(N1)                # the x locations for the groups
-width1 = 0.18                      # the width of the bars
+width1 = 0.22                      # the width of the bars
 
 ind2 = np.arange(N2)                # the x locations for the groups
-width2 = 0.12                      # the width of the bars
+width2 = 0.10                      # the width of the bars
 
-
-#hatch_cycle = cycler('hatch', ['/', '*', '+', '|'])
 
 ## the bars
-rects1 = ax1.bar(ind1+width1, rr, width1,
+rects1 = ax1.bar(ind1, rr, width1,
                 color='firebrick',
 #                yerr=menStd,
                 error_kw=dict(elinewidth=0,ecolor='firebrick'), hatch = '|')
 
-rects2 = ax1.bar(ind1+2*width1, ch, width1,
+rects2 = ax1.bar(ind1+width1, ch, width1,
                     color='gold',
 #                    yerr=womenStd,
                     error_kw=dict(elinewidth=2,ecolor='gold'), hatch='/')
 
-rects3 = ax1.bar(ind1+3*width1, maglev, width1,
+rects3 = ax1.bar(ind1+2*width1, maglev, width1,
                     color='lightpink',
 #                    yerr=animalStd,
                     error_kw=dict(elinewidth=2,ecolor='lightpink'), hatch='.')
 
-rects4 = ax1.bar(ind1+4*width1, sk_ch, width1,
+rects4 = ax1.bar(ind1+3*width1, sk_ch, width1,
                     color='lightgreen',
 #                    yerr=animalStd,
                     error_kw=dict(elinewidth=2,ecolor='lightgreen'), hatch='-')
@@ -120,55 +106,38 @@ def autolabel(rects, ax):
 
         ax.text(rect.get_x() + rect.get_width()/2., label_position,
                 '%d' % int(height),
-                ha='center', va='bottom',**csfont3)					
+                ha='center', va='bottom',**hfont)					
 					
-#autolabel(rects6,ax2)
-#autolabel(rects7, ax2)
-#autolabel(rects8, ax2)
-#autolabel(rects9, ax2)
-#autolabel(rects10, ax2)
-
-ax1.legend(loc='upper center', bbox_to_anchor=(0.5, 1.05),
-          ncol=3, fancybox=True, shadow=True)
-
-# Shrink current axis by 20%
-#box = ax.get_position()
-#ax1.set_position([box.x0, box.y0, box.width * 0.8, box.height])
+#ax1.legend(loc='upper center', bbox_to_anchor=(0.5, 1.05),
+#          ncol=3, fancybox=True, shadow=True)
 
 # axes and labels
 plt.subplot(121)
 ax1.set_xlim(-width1-0.05,len(ind1)+width1)
 ax1.set_ylim(0,10)
-plt.ylabel('# Servers', **csfont)
+plt.ylabel('# Servers', **hfont)
 xTickMarks = ['T1', 'T2', 'T3', 'T4']
-#xTickMarks = ['Campus \nNet 1', 'Campus \nNet 2']
 ax1.set_xticks(ind1+0.50)
-xtickNames = ax1.set_xticklabels(xTickMarks, **csfont2)
+xtickNames = ax1.set_xticklabels(xTickMarks, **hfont)
 ax1.set_axisbelow(True)
 ax1.yaxis.grid(color='gray', linestyle='dashed')
 plt.grid(linestyle='--')
 vals1 = ax1.get_yticks()
-#vals1.yticks(range(0, 8500, 2000), fontsize=46)
-#ax1.set_yticklabels([str(x/1000) +  "k" for x in vals1])
+plt.legend(loc='upper left',fontsize=26,handleheight=2)
 
 plt.subplot(121)
-#ax2.set_xlim(-width2,len(ind2))
-#ax2.yticks(range(0, 170000, 40000), fontsize=46)
 ax2.set_xlim(-2*width2-0.1,len(ind2))
 ax2.set_ylim(0,110)
-#plt.ylabel('# Rules', **csfont)
-#plt.xlabel('Rule placement with 3 Datasets', **csfont)
-xTickMarks = ['Synthesized \n IoT Traffic']
-#xTickMarks = ['Syn. \nCamp 1']
+xTickMarks = ['T4 (Simulated\nSetup)']
 ax2.set_xticks(ind2+0.35)
-xtickNames = ax2.set_xticklabels(xTickMarks, **csfont2)
+xtickNames = ax2.set_xticklabels(xTickMarks, **hfont)
 ax2.set_axisbelow(True)
 ax2.yaxis.grid(color='gray', linestyle='dashed')
-#plt.xlabel('Rule Space Optimization', **csfont)
 plt.grid(linestyle='--')
 vals = ax2.get_yticks()
 #ax2.set_yticklabels([str(x/1000) +  "k" for x in vals])
-ax2.legend(loc='upper right',fontsize=28,handleheight=2)
+ax2.legend(loc='upper right',fontsize=26,handleheight=2)
+plt.xlabel('Traffic Distributions', **hfont)
 
 #plt.legend(loc='upper left')
 #plt.legend(shadow=True, loc=(0.01, 0.78))
