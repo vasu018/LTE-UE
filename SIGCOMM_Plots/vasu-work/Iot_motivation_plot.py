904cc327c46f1da62468c0a6c6cbaf2209cb1812
Poornima Vasudevan <poorni@Poornimas-MacBook-Air.local>
2018-06-21 22:38:19 -0700

    motivation plot

--- SIGCOMM_Plots/vasu-work/Iot_motivation_plot.py
+++ SIGCOMM_Plots/vasu-work/Iot_motivation_plot.py
@@ -11,7 +11,7 @@ from matplotlib.colors import ListedColormap
 sns.set()
 sns.set_style('whitegrid')
 from matplotlib import rcParams
-rcParams.update({'figure.autolayout': True})
+#rcParams.update({'figure.autolayout': True})
 #matplotlib.rcParams['figure.figsize'] = 3, 2
 
 matplotlib.rcParams.update({'font.size':48})
@@ -48,7 +48,7 @@ pivot_df.plot.bar(stacked=True,ax=ax2,
                         ylim=[0, 300000],
                        rot=30,error_kw=dict(elinewidth=2,ecolor='k'), linewidth=2, width=width, capsize=10, position=0, fontsize='32')
 
-ax.set_xticklabels(['12 - 4 am', '4 - 8 am', '8 am - 12 noon','12 noon - 4 pm', '4pm - 8 pm', '8 pm - 12 midnight'])
+ax.set_xticklabels(['12am - 4am', '4am - 8am', '8am - 12pm','12pm - 4pm', '4pm - 8pm', '8pm - 12am'],fontsize = '24',rotation ='0')
 
 #plt.xticks(['12 - 4 am', '4 - 8 am', '8 am - 12 noon','12 noon - 4 pm', '4pm - 8 pm', '8 pm - 12 midnight'])
 #plt.xticks(["12am - 4am", "4am - 8am", "8am - 12noon","12noon - 4pm", "4pm - 8pm", "8pm - 12midnight"])
@@ -61,7 +61,7 @@ pivot_df = pivot_df[col_sequence]
 pivot_df.plot.bar(stacked=True, ax=ax,
                        colormap=ListedColormap(sns.color_palette("GnBu_d")),
                        rot=30, ylim=[0, 50000], error_kw=dict(elinewidth=2,ecolor='k'), linewidth=2, width=width, capsize=10, position =1, fontsize='32')
-#ax.set_xticklabels(['12 - 4 am', '4 - 8 am', '8 am - 12 noon',  '12 noon - 4 pm', '4pm - 8 pm', '8 pm - 12 midnight'])
+ax.set_xticklabels(['12am - 4am', '4am - 8am', '8am - 12pm',  '12pm - 4pm', '4pm - 8pm', '8pm - 12am'],fontsize = '24',rotation ='0')
 ax.set_xlabel('Hours of Day')
 ax.set_ylabel('Number of Control Packets')
 plt.savefig('Iot_motivation_plot.pdf')
