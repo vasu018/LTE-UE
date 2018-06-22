c5bd901cad0420c85423182bffcb0f746678d92a
Poornima Vasudevan <poorni@Poornimas-MacBook-Air.local>
2018-06-21 23:44:51 -0700

    iot motive plot 2

--- SIGCOMM_Plots/vasu-work/iot-motivation-plot2.py
+++ SIGCOMM_Plots/vasu-work/iot-motivation-plot2.py
@@ -25,13 +25,14 @@ fig = plt.figure()
 
 ax = fig.add_subplot(111) 
 ax2 = ax.twinx() 
+fig.subplots_adjust(left=0.1, bottom=0.1, right=0.90)
 
-#df = pd.read_csv('packet_count.txt', sep=' ')
-df = pd.read_csv('graph.txt', sep=' ')
+df = pd.read_csv('packet_count.txt', sep=' ')
+#df = pd.read_csv('graph.txt', sep=' ')
 df.columns = ['Hour', 'Category', 'Data Packets', 'Control Packets']
 df.sort_values(['Data Packets'], inplace=True)
 df['Data Packets'] = df['Data Packets'].astype(int)
-df['Category'] = df['Category'].str.replace('_',' ')
+#df['Category'] = df['Category'].str.replace('_',' ')
 #print (df)
 numHours = max(df['Hour'].values)
 stack = [0] * (numHours + 1)
@@ -43,26 +44,29 @@ pivot_df = df.pivot(index='Hour', columns='Category', values='Data Packets')
 #pivot_df.sort_values(['Data Packets'], inplace=True)
 pivot_df.sort_values(pivot_df.last_valid_index(), axis=1, inplace=True, ascending=False)
 col_sequence = pivot_df.columns
-pivot_df.plot.bar(stacked=True,ax=ax2,
+pivot_df.plot.bar(stacked=True,ax=ax,
                        colormap=ListedColormap(sns.color_palette("GnBu_d")),
                         ylim=[0, 300000],
-                       rot=30,error_kw=dict(elinewidth=2,ecolor='k'), linewidth=2, width=width, capsize=10, position=0, fontsize='32')
+                       rot=30,error_kw=dict(elinewidth=2,ecolor='k'), linewidth=2, width=width, capsize=10, position=0, fontsize='24')
 
 ax.set_xticklabels(['12am - 4am', '4am - 8am', '8am - 12pm','12pm - 4pm', '4pm - 8pm', '8pm - 12am'],fontsize = '24',rotation ='0')
 
 #plt.xticks(['12 - 4 am', '4 - 8 am', '8 am - 12 noon','12 noon - 4 pm', '4pm - 8 pm', '8 pm - 12 midnight'])
 #plt.xticks(["12am - 4am", "4am - 8am", "8am - 12noon","12noon - 4pm", "4pm - 8pm", "8pm - 12midnight"])
-ax2.set_xlabel('Hours of Day')
-ax2.set_ylabel('Number of Data Packets')
+ax2.set_xlabel('Hours of Day', fontsize = '42')
+ax2.set_ylabel('Normalized Data Load', fontsize='42')
+#ax.set_yticks(fontsize='36')
 #plt.savefig('Data.pdf')
 
 pivot_df = df.pivot(index='Hour', columns='Category', values='Control Packets')
 pivot_df = pivot_df[col_sequence]
-pivot_df.plot.bar(stacked=True, ax=ax,
+pivot_df.plot.bar(stacked=True, ax=ax2,
                        colormap=ListedColormap(sns.color_palette("GnBu_d")),
-                       rot=30, ylim=[0, 50000], error_kw=dict(elinewidth=2,ecolor='k'), linewidth=2, width=width, capsize=10, position =1, fontsize='32')
+                       rot=30, ylim=[0, 50000], error_kw=dict(elinewidth=2,ecolor='k'), linewidth=2, width=width, capsize=10, position =1, fontsize='24')
 ax.set_xticklabels(['12am - 4am', '4am - 8am', '8am - 12pm',  '12pm - 4pm', '4pm - 8pm', '8pm - 12am'],fontsize = '24',rotation ='0')
-ax.set_xlabel('Hours of Day')
-ax.set_ylabel('Number of Control Packets')
+ax.set_xlabel('Hours of Day', fontsize='42')
+ax.set_ylabel('Normalized Control Load', fontsize='42')
+ax.legend('left top')
+#ax.set_yticks(fontsize='36')
 plt.savefig('Iot_motivation_plot.pdf')
 plt.show()
